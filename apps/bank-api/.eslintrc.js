62e9a775272d54261971fc9088f1f88961f45e98
muntaxir4 <mjmullick0608@gmail.com>
2024-08-02 00:47:27 +0530

    Init required server, web and database

--- apps/bank-api/.eslintrc.js
+++ apps/bank-api/.eslintrc.js
@@ -1,7 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/server.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,

b04de0dd4daa69ca1707ffc778e1693480d2b50d
muntaxir4 <mjmullick0608@gmail.com>
2024-08-11 23:36:23 +0530

    Fix build errors for user-web

--- apps/user-web/app/(lightTheme)/bank/hdfc/page.tsx
+++ apps/user-web/app/(lightTheme)/bank/hdfc/page.tsx
@@ -1,5 +1,11 @@
 import HDFC from "@/components/app/bank/HDFC";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
 
 export default function HDFCPage() {
-  return <HDFC />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <HDFC />
+    </Suspense>
+  );
 }
